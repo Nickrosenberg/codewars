73cdf2ac285ad9ac86d088598c004d62f1387ca7
Nickrosenberg <beltiukovrosenberg@gmail.com>
2024-12-16 14:07:03 +0300

    even-or-odd

---
+++ 8kye/evenorodd.js
@@ -0,0 +1,14 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+// function evenOrOdd(number) {
+
+// }
+
+function evenOrOdd(number) {
+    // Check if the number is even or odd
+    if (number % 2 === 0) {
+        return "Even"; // Return "Even" for even numbers
+    } else {
+        return "Odd";  // Return "Odd" for odd numbers
+    }
+}
